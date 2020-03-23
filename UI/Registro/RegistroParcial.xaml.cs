0e22418f9e5438f665ced2226146638c353102ea
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-22 23:53:54 -0400

    Agregue las imagenes de los botones como recursos del proyecto

---
+++ UI/Registro/RegistroParcial.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiguelGondresPA2.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for RegistroParcial.xaml
+    /// </summary>
+    public partial class RegistroParcial : Window
+    {
+        public RegistroParcial()
+        {
+            InitializeComponent();
+        }
+    }
+}
