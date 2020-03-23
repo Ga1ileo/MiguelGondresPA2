16e3d30fe15be478ee80077f1fb94bfcae02d2a7
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-22 23:16:54 -0400

    Agregando las entidades Llamadas y Llamadadetalle

---
+++ Entidades/LlamadaDetalle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiguelGondresPA2.Entidades
+{
+    public class LlamadaDetalle
+    {
+
+    }
+}
--- Entidades/Llamadas.cs
+++ Entidades/Llamadas.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MiguelGondresPA2.Entidades
@@ -11,10 +12,15 @@ namespace MiguelGondresPA2.Entidades
         public int LlamadaId { get; set; }
         public string Descripcion { get; set; }
 
+        [ForeignKey("LlamadaId")]
+        public virtual List<LlamadaDetalle> LlamadaDetalle { get; set; }
+
         public Llamadas()
         {
             LlamadaId = 0;
             Descripcion = string.Empty;
+
+            LlamadaDetalle = new List<LlamadaDetalle>();
         }
 
     }
