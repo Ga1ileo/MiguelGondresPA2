4503bd9d5374f83346c7be9d380cdd106abdc63e
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-22 23:59:04 -0400

    Agregue la Validacion

---
+++ Validacion/Validacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace MiguelGondresPA2.Validacion
+{
+    public class Validacion : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+
+            string mensajeDeError = value as string;
+            if(mensajeDeError != null)
+            {
+                if (mensajeDeError.Length <= 0)
+                    return new ValidationResult(false, "Hay algun campo Vacio");
+                return ValidationResult.ValidResult;
+            }
+            return new ValidationResult(false, "Ningun Campo se puede dejar Vacio");
+        }
+    }
+}
