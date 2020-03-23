059f497f3e9f0fd2a1b76788506b17af3de6dba2
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-22 23:25:15 -0400

    Agruegue el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using MiguelGondresPA2.Entidades;
+
+namespace MiguelGondresPA2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Llamadas> llamadas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\RRparcial2.db");
+        }
+
+    }
+}
