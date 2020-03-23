264971f9d69f9e6cf87fe0ebdd932bf5c1facf1b
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-22 23:11:38 -0400

    Add project files.

---
+++ Entidades/Llamadas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MiguelGondresPA2.Entidades
+{
+    public class Llamadas
+    {
+        [Key]
+        public int LlamadaId { get; set; }
+        public string Descripcion { get; set; }
+
+        public Llamadas()
+        {
+            LlamadaId = 0;
+            Descripcion = string.Empty;
+        }
+
+    }
+}
